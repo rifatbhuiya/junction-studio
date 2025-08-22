a935b1dd78c4343a1f84486b25978492070aa579
rifatbhuiya <rifatbhuiya.personal@gmail.com>
2025-08-22 11:17:13 +0600

    added header & hero.slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 (function () {
-    'use-strict';
+    "use-strict";
+
+    // Window on Scroll
+    $(window).on('scroll', function () {
+        let scrolling = $(this).scrollTop();
+        
+        if (scrolling > 50) {
+            $('.main-header').addClass('fixed-header');
+        } else {
+            $('.main-header').removeClass('fixed-header');
+        }
+    });
 }())
